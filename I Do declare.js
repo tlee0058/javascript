b594bdc5c689b6045926d5fd2326639db27c6aef
tlee0058 <32435667+tlee0058@users.noreply.github.com>
2018-01-20 21:11:06 -0500

    Add files via upload

---
+++ I Do declare.js
@@ -0,0 +1,12 @@
+/* Declare variables of the following types:
+4 Numbers
+3 Strings
+2 Booleans
+1 Undefined Variable
+Give the variables descriptive names (ex: string1 and string2)
+Print each one to the console with a description (ex: console.log("The First String:", string1);)
+Try to be creative! (don't use 1, 2, 3, and 4 for your numbers -- test out negatives or decimals!)*/
+
+var num = 4;
+var string = "3";
+var isRed = false;
