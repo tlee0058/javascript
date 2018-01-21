3b3cca8a5438a886f37bdcac8b700ded374fe110
tlee0058 <32435667+tlee0058@users.noreply.github.com>
2018-01-21 10:51:44 -0500

    Add files via upload

---
+++ fancy array.js
@@ -0,0 +1,20 @@
+/* 
+Fancy Array
+Normally, if you print an array such as ["James", "Jill", "Jane", "Jack"], you will see the following:
+
+[ "James", "Jill", "Jane", "Jack" ]
+Let's make it look fancy! Write a function that will make it print like:
+
+0 -> James
+1 -> Jill
+2 -> Jane
+3 -> Jack
+Bonus (Only If You Have Time):
+
+Let the user pass in the symbol that will print (ex: "->", "=>", "::", "-----")
+Have an extra parameter reversed. If the user passes true, print the elements in reverse order*/
+
+var arr = ["James", "Jill", "Jane", "Jack"];
+for (var i = 0; i < arr.length; i++){
+    console.log(i + "--> " + arr[i]);
+}
